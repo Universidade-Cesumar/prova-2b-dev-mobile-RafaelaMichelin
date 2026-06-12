322a351f0d25b61433d78f41d974cc660ed0d172
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-06-11 21:18:27 -0300

    feat: Implementando o método get para realizar o retorno dos materiais já cadastrados.

--- service/api.ts
+++ service/api.ts
@@ -1,1 +1,12 @@
 const API_URL = 'https://6a2b3e5ab687a7d5cbc501a9.mockapi.io/materiais';
+
+export async function getMateriais() {
+//Usando o fetch para integrar com a API
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error('Falha ao buscar materiais');
+  }
+
+  return response.json();
+}
